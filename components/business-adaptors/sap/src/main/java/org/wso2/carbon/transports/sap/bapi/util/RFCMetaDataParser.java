757c95d3653621fbc85ea43df0ac02ddeac11a31
dulanjalidilmi <dulanjalidilmi@gmail.com>
2020-07-17 11:27:47 +0530

    Improve process row element of payloads

--- components/business-adaptors/sap/src/main/java/org/wso2/carbon/transports/sap/bapi/util/RFCMetaDataParser.java
+++ components/business-adaptors/sap/src/main/java/org/wso2/carbon/transports/sap/bapi/util/RFCMetaDataParser.java
@@ -364,6 +364,15 @@ public class RFCMetaDataParser {
                     log.warn("Invalid meta data type element found : " + structureName + " .This meta data " +
                              "type will be ignored");
                 }
+            } else if (qname != null && qname.equals("table")) {
+                String tableName = childElement.getAttributeValue(RFCConstants.NAME_Q);
+                JCoTable jCoTableInner = table.getTable(tableName);
+                if (jCoTableInner != null) {
+                    processTable(childElement, jCoTableInner);
+                } else {
+                    log.warn("Invalid meta data type element found : " + tableName + " .This meta data " +
+                             "type will be ignored");
+                }
             } else {
                 log.warn("Invalid meta data type element found : " + qname + " .This meta data " +
                         "type will be ignored");
